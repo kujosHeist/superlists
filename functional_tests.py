e3ad2e9b195d589cb9824296f01250eebdeaacee
kujosHeist <shane.carty@hotmail.com>
2018-02-26 01:19:59 +0000

    added test for adding more than one item

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# user goes to home page of the app
 		self.browser.get("http://localhost:8000")
@@ -30,10 +35,7 @@ class NewVisitorTest(unittest.TestCase):
 		# "1: Buy milk" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
-
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy milk', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy milk')
 		
 		# There is still a text box asking her to add another item,
 		# She enters "Buy tea"
@@ -42,12 +44,9 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
 
-		
 		# The page updates again and now shows both items
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy milk', [row.text for row in rows])
-		self.assertIn('2: Buy tea', [row.text for row in rows])		
+		self.check_for_row_in_list_table('1: Buy milk')
+		self.check_for_row_in_list_table('2: Buy tea')
 
 		# User notices the site has generated a unique url, and
 		# shows an explanation message
